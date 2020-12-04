6d00bc7ff07ebf167d1de0adeaa436945823bce5
Kat Sauma <katsauma@gmail.com>
2020-12-03 17:12:42 -0800

    added-color-background-buttons

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const theButton = document.getElementById('my-button');
 const theNameSection = document.getElementById('name-display');
 const theInput = document.getElementById('my-input-box');
+const thePinkBackground = document.getElementById('my-pink-button')
+const theLightBlueBackground = document.getElementById('my-light-blue-button')
+const theLightGreenBackground = document.getElementById('my-light-green-button')
+const changeColor = document.getElementById ('greeting')
 
 
 theButton.addEventListener('click', () => {
@@ -8,4 +12,22 @@ theButton.addEventListener('click', () => {
     // whatever code i put in the cool zone will happen on click
     theNameSection.textContent = theInput.value;
     theInput.value = '';
+})
+
+thePinkBackground.addEventListener('click', () => {
+
+    // whatever code i put in the cool zone will happen on click
+    changeColor.style.background = 'pink';
+})
+
+theLightBlueBackground.addEventListener('click', () => {
+
+    // whatever code i put in the cool zone will happen on click
+    changeColor.style.background = 'lightblue';
+})
+
+theLightGreenBackground.addEventListener('click', () => {
+
+    // whatever code i put in the cool zone will happen on click
+    changeColor.style.background = 'lightgreen';
 })
